b6007aebc32c82c6573806c6e08087003cac8cb7
Myself :) <77581682+turkeyncheese@users.noreply.github.com>
2021-03-09 07:53:09 -0800

    Update nonrecfib.py

--- nonrecfib.py
+++ nonrecfib.py
@@ -1,6 +1,6 @@
 FIB = {"max": 1, 0: 0, 1: 1}
 
-#Fibonacci sequence calulated non-recusively and much faster than a, b = 0, 1 method
+#Fibonacci sequence calculated non-recusively, my be faster than a, b = b, a + b method.
 
 def fibonacci(n):
     for i in range(FIB["max"] + 1, n + 1):
