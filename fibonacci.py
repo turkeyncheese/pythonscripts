3da1a55f0c942e9b3613943b3b8f79ae8482138f
Myself :) <77581682+turkeyncheese@users.noreply.github.com>
2021-03-09 07:50:48 -0800

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,16 +1,12 @@
 import sys
 x = int(sys.argv[1])
-n1 = 0
-n2 = 1
+a, b = 0, 1
 
 if x == 0:
-    print(n1)
+    print(a)
 elif x == 1:
-    print(n2)
+    print(b)
 else:
     for i in range(2,x):
-        n3 = n1 + n2 
-        n1 = n2 
-        n2 = n3
-
-print(n3)
+        a, b = b, a + b
+print(a)
