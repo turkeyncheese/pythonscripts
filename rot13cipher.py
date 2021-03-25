5b51b939378a9f1101d5dbf5eb17270fd1ee41bd
Myself :) <77581682+turkeyncheese@users.noreply.github.com>
2021-03-25 10:00:36 -0400

    Update rot13cipher.py

--- rot13cipher.py
+++ rot13cipher.py
@@ -1,3 +1,17 @@
 def rot13(text):
+        alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         out = ''
+        for letter in text:
+                if letter.islower():
+                        index = ord(letter)
+                        newLetter = alpha[((index + 13) % len(alpha))]
+                        out += newLetter.lower()
+                elif letter.isupper():
+                        index = ord(letter)
+                        newLetter = alpha[((index + 13) % len(alpha))]
+                        out += newLetter.upper()
+                else:
+                        out += letter
+        return out
+                        
         
