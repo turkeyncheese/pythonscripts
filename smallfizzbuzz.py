aa43bbd30a2c68b8c52af0d2588f1dd009b5defa
cheese <77581682+turkeyncheese@users.noreply.github.com>
2021-05-22 20:26:29 -0400

    Create smallfizzbuzz.py

---
+++ smallfizzbuzz.py
@@ -0,0 +1,4 @@
+fizzbuzz=lambda f,b,s:[i%f//2*"Fizz"+i%b//4*"Buzz"or-~i for i in range(s)]
+#I believe this may or may not be the smallest solution but I may be wrong, I haven't checked because I was just messing around.
+#Cool thing about this solution is that you can change the variables for what number you divide by for 'Fizz' and 'Buzz'.
+#For example instead of 3 and 5 you can do 12 and 15.
